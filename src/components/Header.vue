317aff4a4996b1b71e0a59ab3cc6f04617134799
Ryan <santoryan1408@gmail.com>
2023-02-20 17:24:44 -0300

    Adicionou responsividade e alterou alguns elementos

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,35 @@
 <script setup>
 import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
 import HomeIcon from './icons/HomeIcon.vue';
 import LibraryIcon from './icons/LibraryIcon.vue';
 import LoginIcon from './icons/LoginIcon.vue';
 import LogOutIcon from './icons/LogOutIcon.vue';
 import SignInIcon from './icons/SignInIcon.vue';
-import ProfileIcon from './icons/ProfileIcon.vue';
 import AddIcon from './icons/AddIcon.vue'
+
+const ul = ref(false);
 
 const props = defineProps({
     userStatus: Object,
     logOut: Function,
     songLibStatus: Function
 })
+
+function showMenu(e) {
+    const menuButton = e.target.parentNode;
+    const menu = menuButton.parentNode.children[1];
+
+    if(!menuButton.classList.contains('active')) {
+        menuButton.classList = 'active';
+        menu.classList = 'active';
+    }
+    else if(menuButton.classList.contains('active')) { 
+        menuButton.classList.remove('active')
+        menu.classList.remove('active')
+    }
+
+}
 
 function endSession () {
     props.logOut();
@@ -23,36 +40,49 @@ function endSession () {
 
 <template>
     <header class="w-full bg-red text-black">
-        <div class="logo">BluOcean</div>
-        <nav>
-            <ul>
-                <li><RouterLink to="/">
+
+        <button id="menuButton" @click="showMenu">
+            <span id="hamburguer"></span>
+        </button>
+
+        <nav ref="ul">
+            <div class="logo">BluOcean</div>
+            <ul class="firstUl">
+                <li>
+                <RouterLink to="/">
                     <HomeIcon/>
                     Home
                 </RouterLink></li>
-                <li><RouterLink to="/loadingLibrary">
+                <li>
+
+                <RouterLink to="/loadingLibrary">
                     <LibraryIcon/>
                     Your Songs
                 </RouterLink></li>
-                <li v-if="props.userStatus.isLoged === false"><RouterLink to="/login">
+
+                <li v-if="props.userStatus.isLoged === false">
+                <RouterLink to="/login">
                     <LoginIcon/>
                     <span>Login</span>
                 </RouterLink></li>
-                <li v-else @click="endSession"><a>
+                <li v-else @click="endSession">
+
+                <a>
                     <LogOutIcon/>
                     <span>Log Out</span>
                 </a></li>
-                <li v-if="props.userStatus.isLoged === false"><RouterLink to="/register">
+                <li v-if="props.userStatus.isLoged === false">
+
+                <RouterLink to="/register">
                     <SignInIcon/>
                     Register
                 </RouterLink></li>
-                <li v-else><a>
-                    <ProfileIcon/>
-                    {{ props.userStatus.userName }}
-                </a></li>
+
             </ul>
+
             <ul>
-                <li><RouterLink to="/addSong">
+                <li>
+                <RouterLink to="/addSong">
                     <AddIcon/>
                     Add Song
                 </RouterLink></li>
---
+++ src/components/icons/MenuIcon.vue
@@ -0,0 +1,8 @@
+<template>
+    <svg width={46} height={46} fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+        strokeWidth={2.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M5 7h14" />
+        <path d="M5 12h14" />
+        <path d="M5 17h14" />
+    </svg>
+</template>
